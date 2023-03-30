915fda42054030f6343d48cb327bbc69e9adfe3e
Nicolas Danelon <nicolasdanelon@gmail.com>
2023-03-30 11:47:35 -0300

    🐛 fix(search.js): fix typo in error log message ✨ feat(search.js): add validation for empty search parameter and return 404 status code

--- search.js
+++ search.js
@@ -1,20 +1,19 @@
 const search = (req, res) => {
-    // QUERY STRING
-    const query = JSON.stringify(req.query.q);
-    console.log(`[INFO] Searching for ${query || null}`);
+  // QUERY STRING
+  const query = JSON.stringify(req.query.q);
+  console.log(`[INFO] Searching for ${query || null}`);
 
+  if (query == "") {
+    console.log("[ERRO] No valid search parameter");
+    return res.send({ error: "no valid search parameter" }).status(404);
+  }
 
-    if (query == "") {
-        console.log("[ERRO] No valid search parameter");
-        return res.send({ error: "no valid search parameter" }).status(404);
-    }
-
-    return res.json({
-        results: [
-            { name: "algo", id: 4 },
-            { name: "otro", id: 5 },
-        ],
-    });
+  return res.json({
+    results: [
+      { name: "algo", id: 4 },
+      { name: "otro", id: 5 },
+    ],
+  });
 }
 
 module.exports = search
